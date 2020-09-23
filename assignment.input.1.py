46fc196dd3d4b76de984e5e9f6711f802d06264c
Pinky-cloud224 <pinkydebnath4@gmail.com>
2020-09-23 13:08:40 +0600

    Imput Commit

---
+++ assignment.input.1.py
@@ -0,0 +1,68 @@
+#Example : 01
+#--------------------------------------
+#Let take 2 integers:
+
+a=input(int())
+b=input(int())
+
+#relation one
+
+if a>b:
+    print("a is bigger")
+elif a<b:
+    print("b is bigger")
+elif a==b:
+    print("Two are equal")
+else:
+    print("Nothing")
+
+#relation two
+
+if a>b:
+    print("a is bigger")
+elif a<b:
+    print("b is greater")
+elif a==b:
+    print("Two are equal")
+else:
+    print("Noo")
+
+#relation three:
+
+if a>b:
+    print("a is greater")
+elif b<a:
+    print("b is large")
+elif b==a:
+    print("Same")
+elif b!=a:
+    print("Not Equal")
+elif b>=a:
+    print("b is equal or large")
+elif a>=b:
+    print("a is bigger or equal")
+elif a%b==0:
+    print("Reminder")
+else:
+    print("Something Else")
+
+#relation four:
+if b>=a:
+    print("Equal and large")
+elif a>b:
+    print("a is big")
+elif a>=b:
+    print("a is equally big")
+else:
+    print("New")
+
+-------------------------------------
+
+#relation five:
+    if a/b==17:
+        print("Devide")
+    else:
+        print("Anything")
+------------------------------------------------------------
+        
+    
